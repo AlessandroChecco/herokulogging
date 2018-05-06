16295bd656c27c5d0a2f559e1cf13a4744c9fa26
Alessandro Checco <a.checco@sheffield.ac.uk>
2018-05-06 03:25:12 +0100

    Update clock.py

--- clock.py
+++ clock.py
@@ -4,10 +4,10 @@ import os
 
 sched = Scheduler()
 
-@sched.interval_schedule(minutes=1)
+@sched.interval_schedule(minutes=10)
 def timed_job():
     r = requests.get(os.environ.get('CURRENTDOMAIN'))
-    print 'KEEPING ALIVE'
+    print 'KEEPING ALIVE: '+os.environ.get('CURRENTDOMAIN')
     print r.status_code
 
 sched.start()
